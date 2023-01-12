f994ea2fe8044505a5487428a3491d0dd18722d8
Lucas Espinar <espinar.lucas0@gmail.com>
2023-01-12 16:24:27 +0100

    Création de module de pile

--- main.c
+++ main.c
@@ -1,76 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "pile.h"
 
-struct cellule
-{
-    int elem;
-    struct cellule *suiv;
-};
-
-typedef struct cellule *T_pile;
-
-T_pile creerVide() {
-    T_pile p = NULL;
-    return p;
-}
-
-T_pile empiler(T_pile p, int val) {
-    T_pile newcase = (T_pile)malloc(sizeof(struct cellule));
-    if (newcase == NULL){
-        fprintf (stderr, "Memoire insuffisante\n");
-        exit(EXIT_FAILURE);
-    }
-    else {
-        newcase->elem = val;
-        newcase->suiv = p;
-        return newcase;
-    }
-}
-
-T_pile depiler(T_pile p) {
-    if (p == NULL)
-    {
-        fprintf(stderr, "Vide\n");
-        exit(EXIT_FAILURE);
-    }
-    else {
-        T_pile newcase = (T_pile)malloc(sizeof(struct cellule));
-        T_pile suivant = p -> suiv;
-        newcase->suiv = suivant->suiv;
-        newcase->elem = suivant->elem;
-        free(p);
-        free(suivant);
-        return newcase;
-    }
-}
-
-void afficherPile(T_pile p) {
-    if (p == NULL)
-    {
-        fprintf(stderr, "Vide\n");
-        exit(EXIT_FAILURE);
-    }
-    else 
-    {
-        T_pile actuel = p;
-        while(actuel != NULL) {
-
-            printf("%d <- ", actuel->elem);
-            actuel = actuel -> suiv;
-        }
-        printf("FIN");
-    }
-}
-
-int main()
+int main(int argc, char *argv[])
 {
     T_pile a;
     a = creerVide();
-    a = empiler(a, 12);
-    printf("%d \n", a->elem);
+    for (int i = 1; i <= 50; i++) {
+        a = empiler(a, i);
+    }
 
-    a = empiler(a, 16);
-    printf("%d \n", a->elem);
-
-
+    afficherPile(a);
 }
