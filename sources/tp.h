ea4736606b9492222eb37dfb1b40110dcb009cb2
Lucas Espinar <espinar.lucas0@gmail.com>
2023-01-18 00:43:18 +0100

    Erreur coté Lucas

--- sources/tp.h
+++ sources/tp.h
@@ -5,10 +5,4 @@ int division();
 int multi_aux(int a, int b);
 int multi();
 int pyramide();
-<<<<<<< HEAD
 int factoriel();
-=======
-int factoriel();
-
-
->>>>>>> 51da25179c9639ed44577419bb14bd226a15e85a
