1a4c2826eb10936bbda3d838e5876b6c3fb14dc9
Lucas Espinar <espinar.lucas0@gmail.com>
2023-01-19 22:24:49 +0100

    Ajout des arbres

--- main.c
+++ main.c
@@ -4,16 +4,8 @@
 #include "sources/file.h"
 #include "sources/tp.h"
 #include "sources/shaya.h"
+#include "sources/tree.h"
 
 int main(){
-
-    T_file a;
-    enfiler(&a, 6);
-    enfiler(&a, 8);
-    enfiler(&a, 9);
-    afficher_tete_queue(a);
-    defiler(&a);
-    afficher_tete_queue(a);
-    return 0;
-    
+    return test_tree();   
 }
---
+++ sources/tree.h
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+typedef struct tree *T_btree;
+
+T_btree creer_arbre_vide();
+T_btree creer_arbre(int val);
+T_btree creer_arbre_test(int profondeur);
+T_btree subleft(T_btree tree);
+void afficher_arbre(T_btree tree);
+int free_branch(T_btree tree);
+void free_tree(T_btree tree);
+void cut(T_btree tree, char l_or_r);
+unsigned long long get_nbr_leaf(T_btree tree);
+unsigned long long get_nbr_noeuds(T_btree tree);
+bool isleaf(T_btree tree);
+int test_tree();
