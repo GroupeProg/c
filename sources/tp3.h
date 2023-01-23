176f2d02cb32798d22f42dcde69d48d756388a3b
ShayaJS <64969145+Shayajs@users.noreply.github.com>
2023-01-23 17:47:54 +0100

    Update tp3.h

--- sources/tp3.h
+++ sources/tp3.h
@@ -1,2 +1,40 @@
-typedef struct date Date;
-void afficheDate(Date D);
+#ifndef TP3_H_INCLUDED
+#define TP3_H_INCLUDED
+
+typedef enum{
+    lundi       = 1,
+    mardi       = 2,
+    mercredi    = 3,
+    jeudi       = 4,
+    vendredi    = 5,
+    samedi      = 6,
+    dimanche    = 7
+}T_jour;
+
+typedef enum {
+    janvier     = 1,
+    fevrier     = 2,
+    mars        = 3,
+    avril       = 4,
+    mai         = 5,
+    juin        = 6,
+    juillet     = 7,
+    aout        = 8,
+    septembre   = 9,
+    octobre     = 10,
+    novembre    = 11,
+    decembre    = 12
+}T_mois;
+
+typedef struct{
+
+    T_jour jour;
+    T_mois mois;
+    int annee;
+
+} T_date;
+
+void afficheDate(T_date d);
+T_date dateVide( void );
+
+#endif
