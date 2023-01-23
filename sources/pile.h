b61e89924492ae965885d2a498fefb7f87e7e207
ShayaJS <64969145+Shayajs@users.noreply.github.com>
2023-01-23 18:06:58 +0100

    Update pile.h

--- sources/pile.h
+++ sources/pile.h
@@ -1,6 +1,11 @@
+#ifndef PILE_H_INCLUDED
+#define PILE_H_INCLUDED
+
 typedef struct cellule *T_pile;
 T_pile creerVide(void);
 T_pile empiler(T_pile p, int val);
 T_pile depiler(T_pile p);
 void afficherPile(T_pile p);
 int lenPile(T_pile p);
+
+#endif
