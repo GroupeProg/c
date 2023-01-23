b8e2c4f0a01e5e608ffa23f760a47a73e96568b7
ShayaJS <64969145+Shayajs@users.noreply.github.com>
2023-01-23 17:47:20 +0100

    Update shaya.h

--- sources/shaya.h
+++ sources/shaya.h
@@ -1,10 +1,19 @@
-struct array
+#ifndef SHAYA_H_INCLUDED
+#define SHAYA_H_INCLUDED
+
+
+typedef struct
 {
-    int elem;
+    int *elem;
     int size;
-};
-typedef struct array Array;
-Array empty_Array();
-void append_int_array(Array *a, int e);
-void afficher_array(Array a);
-void free_array(Array *a);
+} T_Array;
+
+//typedef Array *T_Array;
+
+
+T_Array init_Array(int taille);
+void append_int_array(T_Array *a, int e);
+void afficher_array(T_Array a);
+void free_array(T_Array *a);
+
+#endif
