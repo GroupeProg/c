871e20cb289d2dbd1b2d8a3b3ab83f2535e54de5
ShayaJS <64969145+Shayajs@users.noreply.github.com>
2023-01-23 17:48:14 +0100

    Update tp3.c

--- sources/tp3.c
+++ sources/tp3.c
@@ -4,41 +4,19 @@
 #include <windows.h>
 #include <math.h>
 
-enum jour {
-    lundi       = 1,
-    mardi       = 2,
-    mercredi    = 3,
-    jeudi       = 4,
-    vendredi    = 5,
-    samedi      = 6,
-    dimanche    = 7
-    };
+#include "tp3.h"
 
-enum mois {
-    janvier     = 1,
-    fevrier     = 2,
-    mars        = 3,
-    avril       = 4,
-    mai         = 5,
-    juin        = 6,
-    juillet     = 7,
-    aout        = 8,
-    septembre   = 9,
-    octobre     = 10,
-    novembre    = 11,
-    decembre    = 12
-    };
 
-struct date{
-    enum jour jour;
-    enum mois mois;
-    int annee;
-};
 
-typedef struct date Date;
-
-void afficheDate(Date d) {
+void afficheDate(T_date d) {
     printf("%d/", d.annee);
-    printf("%d/", d.mois);
-    printf("%d/", d.jour);
+    printf("%d/", (int)d.mois);
+    printf("%d/", (int)d.jour);
 }
+
+
+/*
+T_date dateVide( void ){
+
+}
+*/
