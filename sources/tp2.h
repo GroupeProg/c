75e49b2064400ac63b8e9b089dc107789aeb2b14
Rackam06 <122537405+Rackam06@users.noreply.github.com>
2023-02-01 17:18:35 +0100

    Update tp2.h

--- sources/tp2.h
+++ sources/tp2.h
@@ -1,13 +1,21 @@
 #ifndef TP2_H_INCLUDED
 #define TP2_H_INCLUDED
-#endif // TP2_H_INCLUDED
-
-/* Tableaux */
-
-int random();
-int tq1();
 
 /* Fonctions */
 
+int ligne();
+int moyenne();
+int polynome();
+int random();
 int tab2dim();
-int moy_gen_etu();
+int moy_gen();
+int m_note();
+
+/* Fonctions recursives */
+
+int facto();
+void facto_afficheur();
+int fibo();
+void fibonacci_tp2_afficheur();
+
+#endif // TP2_H_INCLUDED
