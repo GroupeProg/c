4464d0daeee81df8feabb5aa80216409575a1101
Rackam06 <122537405+Rackam06@users.noreply.github.com>
2023-02-07 15:33:56 +0100

    Update tp3.h

--- sources/tp3.h
+++ sources/tp3.h
@@ -1,63 +1,42 @@
 #ifndef TP3_H_INCLUDED
 #define TP3_H_INCLUDED
 
-// Structures de données
+typedef enum{lundi = 1, mardi = 2, mercredi = 3, jeudi = 4, vendredi = 5, samedi = 6, dimanche = 7}T_jour;
 
-typedef enum{
-    lundi       = 1,
-    mardi       = 2,
-    mercredi    = 3,
-    jeudi       = 4,
-    vendredi    = 5,
-    samedi      = 6,
-    dimanche    = 7
-}T_jour;
+// Q1 : type énuméré des jours de la semaine
 
-typedef enum {
-    janvier     = 1,
-    fevrier     = 2,
-    mars        = 3,
-    avril       = 4,
-    mai         = 5,
-    juin        = 6,
-    juillet     = 7,
-    aout        = 8,
-    septembre   = 9,
-    octobre     = 10,
-    novembre    = 11,
-    decembre    = 12
-}T_mois;
+typedef enum{janvier = 1, fevrier = 2, mars = 3, avril = 4, mai = 5, juin = 6, jullet = 7, aout = 8, septembre = 9, octobre = 10, novembre = 11, decembre = 12}T_mois;
+
+// Q2 : meme chose avec les mois
 
 typedef struct{
-
     T_jour jour;
+    int njour;
     T_mois mois;
-    int jour_mois;
     int annee;
+}T_date;
 
-} T_date;
+// Q3 : type structuré pour représenter une date
 
-void afficheDate(T_date d);
-T_date saisieDate(int jour_du_mois, T_mois mois, int annee);
-void afficheDateWithDay(T_date date);
-T_date date_zero(void);
-int bissextile(int annee);
-int get_nbr_jour(T_jour mois, int annee);
-const char *get_day_name(T_jour jour);
-const char *get_mounth_name(T_jour mois);
-int get_days_from_date_zero(T_date date);
-int get_day_enum(T_date date);
-T_date saisieDate_with_prompt(void);
-void modifie_annee(T_date *date, int annee);
-void calendar(T_mois mois, int annee);
+/* FONCTIONS */
 
-// Pointeurs
+int convert_jour();
+int convert_mois();
+void afficheDate();
+void saisieDate();
+void saisieDateMenu();
 
-void pointeurs();
-void tab_with_pointeur();
+/* POINTEURS */
 
-// Tableaux bis repetita
+int affichePoint();
+int tabPoint();
+int comparePoint();
+int *alloueDate();
+
+/* TABLEAUX BIS REPETITA */
 
 float *allocTabFloat(int size);
+allocTabFloatPrompt();
 
-#endif
+
+#endif // TP3_H_INCLUDED
